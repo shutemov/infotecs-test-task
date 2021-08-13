153a5d1dfbb0648174cedae2bf39457e8d5591fb
Shutemov <shutemov1997@mail.ru>
2021-08-13 12:01:18 +0300

    Получает всех пользователей

--- SortingActions.js
+++ SortingActions.js
@@ -1,2 +1,5 @@
 import { getAllUsers } from "./DataManager.js";
 import { insertDataIntoTable } from "./TableManager.js";
+
+const users = getAllUsers();
+console.log("users from data manager", users);
