49f15f0e1a1f959efae5002bd85831a87a89b3fa
Shutemov <shutemov1997@mail.ru>
2021-08-13 12:34:19 +0300

    Реализует функцию setUpTableHeaderAction
    Теперь существует 1 листенер, который отслеживает клик по заголовку и, в зависимости от его типа (столбца), применяет стратегию сортировки

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ import { insertDataIntoTable } from "./TableManager.js";
 
 
 
+const setUpTableHeaderAction = () => {
+  const tableHeader = document.querySelector(".users-table__header-row");
+
+  tableHeader.addEventListener("click", (event) => {
+    const isFirstNameColumn = event.target.classList.contains(
+      "users-table__first-column-head"
+    );
+
+    const isLastNameColumn = event.target.classList.contains(
+      "users-table__second-column-head"
+    );
+
+    const isAboutColumn = event.target.classList.contains(
+      "users-table__third-column-head"
+    );
+
+    const isEyeColorColumn = event.target.classList.contains(
+      "users-table__fourth-column-head"
+    );
+
+    if (isFirstNameColumn) sortFirstNameByAlphabet();
+    if (isLastNameColumn) sortLastNameByAlphabet();
+    if (isAboutColumn) sortAboutByAlphabet();
+    if (isEyeColorColumn) sortEyeColorByAlphabet();
   });
 };
 
