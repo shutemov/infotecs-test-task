95ba913f6b246cd573e5be5166f3f5613a7e07d5
Shutemov <shutemov1997@mail.ru>
2021-08-13 18:36:55 +0300

    Добавляет getUsersById в dataManager

--- DataManager.js
+++ DataManager.js
@@ -3,4 +3,11 @@ import { data as users } from "./data.js";
 export const getAllUsers = () => {
   return users;
 };
+
+export const getUserById = (id) => {
+  return users.find((user) => {
+    return user.id === id;
+  });
+};
+
 };
