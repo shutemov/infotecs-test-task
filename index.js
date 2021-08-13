5be07f0d517c7784f35b5d9dab65962b231fccba
Shutemov <shutemov1997@mail.ru>
2021-08-13 12:28:23 +0300

    Удаляет функцию buildContent из index.js
    Теперь это внутренняя функция в TableManager

--- index.js
+++ index.js
@@ -6,27 +6,12 @@ import {
   sortEyeColorByAlphabet,
 } from "./SortingActions.js";
 
-const buildContent = (data) => {
-  let content = "";
+console.log("Users from index", users);
 
-  data.forEach((element) => {
-    const { id, name, about, eyeColor } = element;
-    const { firstName, lastName } = name;
-    const thirdColumnStyle = `users-table__third-column-body`;
 import { insertDataIntoTable } from "./TableManager.js";
 
-    content += `<tr data-id=${id}>`;
 
-    content += `<td>${firstName}</td>`;
-    content += `<td>${lastName}</td>`;
-    content += `<td class=${thirdColumnStyle}> ${about} </td>`;
-    content += `<td>${eyeColor}</td>`;
 
-    content += `</tr>`;
-  });
-
-  return content;
-};
 
 const insertDataIntoTable = (data) => {
   const tbody = document.querySelector(".users-table>tbody");
