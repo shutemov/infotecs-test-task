aa3228eea3cd2664ed0adc6a0ed6654b2476f5e3
Shutemov <shutemov1997@mail.ru>
2021-08-13 12:32:50 +0300

    Удаляет старый алфавитный компаратор

--- index.js
+++ index.js
@@ -13,22 +13,6 @@ import { insertDataIntoTable } from "./TableManager.js";
 
 
   });
-const alphabetComparatorByProp = (...props) => {
-  if (props.length > 1) {
-    const [propLevel1, propLevel2] = props;
-    return (a, b) => {
-      if (a[propLevel1][propLevel2] < b[propLevel1][propLevel2]) return -1;
-      if (a[propLevel1][propLevel2] > b[propLevel1][propLevel2]) return 1;
-      return 0;
-    };
-  }
-
-  const [prop] = [props];
-  return (a, b) => {
-    if (a[prop] < b[prop]) return -1;
-    if (a[prop] > b[prop]) return 1;
-    return 0;
-  };
 };
 
 insertDataIntoTable(users);
