9b1548a5434cdbf231eabe1f79d25016286c7637
Shutemov <shutemov1997@mail.ru>
2021-08-13 18:33:53 +0300

    Добавляет функцию получения инпутов формы

--- form.js
+++ form.js
@@ -1,1 +1,11 @@
 
+const getInputFields = () => {
+  const nodeListEditFormItems = document.querySelectorAll(".edit-form__item");
+
+  const formInputFields = Array.from(nodeListEditFormItems).map((item) => {
+    // each 3 elem is input or textarea
+    return item.childNodes[3];
+  });
+
+  return formInputFields;
+};
