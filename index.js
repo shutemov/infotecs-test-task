772c366cebf3518f7757b9e9b9f843c294e6ca4d
Shutemov <shutemov1997@mail.ru>
2021-08-13 18:42:26 +0300

    Добавляет листенер save кнопки формы в index.js

--- index.js
+++ index.js
@@ -52,4 +52,30 @@ const setUpTableRowAction = () => {
     storage.setItem("userId", userId);
   });
 };
+
+const setUpSaveAction = () => {
+  const saveButton = document.querySelector(".edit-form__save-button");
+
+  saveButton.addEventListener("click", () => {
+    const storage = window.localStorage;
+    const id = storage.getItem("userId");
+
+    if (!id) alert("user not selected");
+
+    const user = getUserById(id);
+
+    const { firstName, lastName, about, eyeColor } = getDataFromForm();
+
+    user.name.firstName = firstName;
+    user.name.lastName = lastName;
+    user.about = about;
+    user.eyeColor = eyeColor;
+
+    updateUser(user);
+    insertDataIntoTable(users);
+    clearForm()
+    storage.clear()
+  });
+};
+
 setUpTableHeaderAction();
