093a1d58be2a313538919bdc6c7437f3e9b774f0
Shutemov <shutemov1997@mail.ru>
2021-08-13 11:51:38 +0300

    Реализует функцию создания содержимого таблицы

--- TableManager.js
+++ TableManager.js
@@ -1,4 +1,22 @@
 export const insertDataIntoTable = (data) => {
 };
 const buildContent = (data) => {
+  let content = "";
+
+  data.forEach((element) => {
+    const { id, name, about, eyeColor } = element;
+    const { firstName, lastName } = name;
+    const thirdColumnStyle = `users-table__third-column-body`;
+
+    content += `<tr data-id=${id}>`;
+
+    content += `<td>${firstName}</td>`;
+    content += `<td>${lastName}</td>`;
+    content += `<td class=${thirdColumnStyle}> ${about} </td>`;
+    content += `<td>${eyeColor}</td>`;
+
+    content += `</tr>`;
+  });
+
+  return content;
 };
