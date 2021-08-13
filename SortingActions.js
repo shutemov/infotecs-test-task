b94f25cea4f2ea90f9f197db4b1805ac82152dc7
Shutemov <shutemov1997@mail.ru>
2021-08-13 12:04:57 +0300

    Реализует алфавитный компаратор по firstName

--- SortingActions.js
+++ SortingActions.js
@@ -4,4 +4,12 @@ import { insertDataIntoTable } from "./TableManager.js";
 const users = getAllUsers();
 console.log("users from data manager", users);
 const firstNameComparatorByAlphabet = (...path) => {
+  const [name, firstName] = path;
+  return (a, b) => {
+    if (a[name][firstName] < b[name][firstName]) return -1;
+    if (a[name][firstName] > b[name][firstName]) return 1;
+    return 0;
+  };
+};
+
 };
