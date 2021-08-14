3c94b1cc9d3279bbca331ad41c4bd28910c7ef48
Shutemov <shutemov1997@mail.ru>
2021-08-14 08:48:52 +0300

    Добавляет функцию установки листнера на хедер таблицы

--- setUpTable.js
+++ setUpTable.js
@@ -6,6 +6,28 @@ import {
 } from "./columnsActions.js";
 import { getUserById } from "./dataManager.js";
 import { setDataIntoForm } from "./form.js";
+const setUpTableHeaderAction = (className) => {
+  const tableHeader = document.querySelector(`.${className}__header-row`);
+
+  tableHeader.addEventListener("click", (event) => {
+    const firstColumnClassName = `${className}__first-column-head`;
+    const isFirstNameColumn =
+      event.target.classList.contains(firstColumnClassName);
+
+    const secondColumnClassName = `${className}__second-column-head`;
+    const isLastNameColumn = event.target.classList.contains(
+      secondColumnClassName
+    );
+
+    const thirdColumnClassName = `${className}__third-column-head`;
+    const isAboutColumn = event.target.classList.contains(thirdColumnClassName);
+
+    const fourthColumnClassName = `${className}__fourth-column-head`;
+    const isEyeColorColumn = event.target.classList.contains(
+      fourthColumnClassName
+    );
+
+    if (isFirstNameColumn) updateTableByFirstNameAlphabetSorting(className);
     if (isLastNameColumn) updateTableByLastNameAlphabetSorting(className);
     if (isAboutColumn) updateTableByAboutAlphabetSorting(className);
     if (isEyeColorColumn) updateTableByEyeColorAlphabetSorting(className);
