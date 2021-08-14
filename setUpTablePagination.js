1035c858f9241134aa01a413b638fd024b259322
Shutemov <shutemov1997@mail.ru>
2021-08-14 20:36:26 +0300

    Добавляет setUpPaginationListener в setUpTablePagination
    Добавляет листенер на div с пагинацией, следит за перемещением вперед\назад по страницам

--- setUpTablePagination.js
+++ setUpTablePagination.js
@@ -1,1 +1,17 @@
 import { doBack, doNext, setNumOfEntries } from "./tablePagination.js";
+const setUpPaginationListener = (targetTableClassname) => {
+  const tablePagination = document.querySelector(
+    `.${targetTableClassname}-pagination`
+  );
+
+  tablePagination.addEventListener("click", (event) => {
+    const backButtonClassName = `${targetTableClassname}-pagination__backward`;
+    const isBackButton = event.target.classList.contains(backButtonClassName);
+
+    const nextButtonClassName = `${targetTableClassname}-pagination__forward`;
+    const isNextButton = event.target.classList.contains(nextButtonClassName);
+
+    if (isNextButton) doNext();
+    if (isBackButton) doBack();
+  });
+};
