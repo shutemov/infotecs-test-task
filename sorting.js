0330ab66a140ae2148c61f8da82ff7f33e42692d
Shutemov <shutemov1997@mail.ru>
2021-08-14 16:37:12 +0300

    Удаляет компараторы в sroting.js

--- sorting.js
+++ sorting.js
@@ -19,46 +19,8 @@ export const sortByEyeColor = () => {
   return users.sort(eyeColorComparatorByAlphabet("eyeColor"));
 };
 
-
-
 const alphabetComparator = (a, b) => {
   if (a < b) return -1;
   if (a > b) return 1;
   return 0;
-};
-
-const firstNameComparatorByAlphabet = (...path) => {
-  const [name, firstName] = path;
-  return (a, b) => {
-    if (a[name][firstName] < b[name][firstName]) return -1;
-    if (a[name][firstName] > b[name][firstName]) return 1;
-    return 0;
-  };
-};
-
-const lastNameComparatorByAlphabet = (...path) => {
-  const [name, lastName] = path;
-  return (a, b) => {
-    if (a[name][lastName] < b[name][lastName]) return -1;
-    if (a[name][lastName] > b[name][lastName]) return 1;
-    return 0;
-  };
-};
-
-const aboutComparatorByAlphabet = (...path) => {
-  const [about] = path;
-  return (a, b) => {
-    if (a[about] < b[about]) return -1;
-    if (a[about] > b[about]) return 1;
-    return 0;
-  };
-};
-
-const eyeColorComparatorByAlphabet = (...path) => {
-  const [eyeColor] = path;
-  return (a, b) => {
-    if (a[eyeColor] < b[eyeColor]) return -1;
-    if (a[eyeColor] > b[eyeColor]) return 1;
-    return 0;
-  };
 };
