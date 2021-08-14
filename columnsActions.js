a0bfe20aacddf6fbcd491dedb1eb890606bcc7aa
Shutemov <shutemov1997@mail.ru>
2021-08-14 08:43:13 +0300

    Обновляет экшены столбцов таблицы в columnsActions.js

--- columnsActions.js
+++ columnsActions.js
@@ -9,3 +9,23 @@ import {
 
 const users = getAllUsers();
 console.log("users from data manager", users);
+
+export const updateTableByFirstNameAlphabetSorting = (targetTableClassName) => {
+  const sortedUsers = sortByFirstName();
+  insertDataIntoTable(targetTableClassName, sortedUsers);
+};
+
+export const updateTableByLastNameAlphabetSorting = (targetTableClassName) => {
+  const sortedUsers = sortByLastName();
+  insertDataIntoTable(targetTableClassName, sortedUsers);
+};
+
+export const updateTableByAboutAlphabetSorting = (targetTableClassName) => {
+  const sortedUsers = sortByAbout();
+  insertDataIntoTable(targetTableClassName, sortedUsers);
+};
+
+export const updateTableByEyeColorAlphabetSorting = (targetTableClassName) => {
+  const sortedUsers = sortByEyeColor();
+  insertDataIntoTable(targetTableClassName, sortedUsers);
+};
