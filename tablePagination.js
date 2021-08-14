7e1a854310ff25708212be25042c24d162614076
Shutemov <shutemov1997@mail.ru>
2021-08-14 20:32:33 +0300

    Добавляет функцию doNext в tablePagination.js

--- tablePagination.js
+++ tablePagination.js
@@ -4,3 +4,14 @@ import { insertDataIntoTable } from "./table.js";
 let page = 0;
 let numOfEntries = 1;
 
+export const doNext = () => {
+  page++;
+  const start = page * numOfEntries;
+  const end = start + numOfEntries;
+  const users = getUsersByInterval(start, end);
+
+  const tableClassName = `users-table`;
+  insertDataIntoTable(tableClassName, users);
+
+  return users;
+};
