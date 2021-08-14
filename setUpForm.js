886ce54c1959950330040fd757730024b8cc2b21
Shutemov <shutemov1997@mail.ru>
2021-08-14 08:53:04 +0300

    Добавляет приват. функ. установ. листенера на save butt.

--- setUpForm.js
+++ setUpForm.js
@@ -1,3 +1,28 @@
 import { getDataFromForm, clearForm } from "./form.js";
 import { getUserById, updateUser } from "./dataManager.js";
 
+const setUpFormSaveButtonAction = (buttonClassName) => {
+  const saveButton = document.querySelector(buttonClassName);
+
+  saveButton.addEventListener("click", () => {
+    const storage = window.localStorage;
+    const id = storage.getItem("userId");
+
+    if (!id) alert("user not selected");
+
+    const user = getUserById(id);
+
+    const { firstName, lastName, about, eyeColor } = getDataFromForm();
+
+    user.name.firstName = firstName;
+    user.name.lastName = lastName;
+    user.about = about;
+    user.eyeColor = eyeColor;
+
+    updateUser(user);
+    // insertDataIntoTable(users);
+    clearForm();
+    storage.clear();
+  });
+};
+
