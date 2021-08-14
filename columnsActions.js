fd853ad59865d98f51746db4003b566190691d84
Shutemov <shutemov1997@mail.ru>
2021-08-14 17:55:17 +0300

    Добавляет функцию обновления сурс данных после сортировки

--- columnsActions.js
+++ columnsActions.js
@@ -12,20 +12,24 @@ console.log("users from data manager", users);
 
 export const updateTableByFirstNameAlphabetSorting = (targetTableClassName) => {
   const sortedUsers = sortByFirstName();
+  updateAllUsers(sortedUsers);
   insertDataIntoTable(targetTableClassName, sortedUsers);
 };
 
 export const updateTableByLastNameAlphabetSorting = (targetTableClassName) => {
   const sortedUsers = sortByLastName();
+  updateAllUsers(sortedUsers);
   insertDataIntoTable(targetTableClassName, sortedUsers);
 };
 
 export const updateTableByAboutAlphabetSorting = (targetTableClassName) => {
   const sortedUsers = sortByAbout();
+  updateAllUsers(sortedUsers);
   insertDataIntoTable(targetTableClassName, sortedUsers);
 };
 
 export const updateTableByEyeColorAlphabetSorting = (targetTableClassName) => {
   const sortedUsers = sortByEyeColor();
+  updateAllUsers(sortedUsers);
   insertDataIntoTable(targetTableClassName, sortedUsers);
 };
