ab3ad9ae79e4320c63c1a20576e9252c9f4f822a
Shutemov <shutemov1997@mail.ru>
2021-08-14 20:43:00 +0300

    Обновляет index.js
    Добавляет тестовую конфигурацию пагинации

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import { getAllUsers, getUsersByInterval } from "./data.js";
 import { insertDataIntoTable } from "./table.js";
 import { setUpTableActions } from "./setUpTable.js";
 import { setUpFormActions } from "./setUpForm.js";
+import { setUpPagination } from "./setUpTablePagination.js";
+import { getUsersByCurrentPage } from "./tablePagination.js";
 
-const users = getAllUsers();
+//test
+setUpPagination();
+
+const users = getUsersByCurrentPage();
 console.log("Users from index", users);
 
 const tableClassName = "users-table";
 setUpTableActions(tableClassName);
 insertDataIntoTable(tableClassName, users);
 
-const newTableClassName = "users-table-1";
-setUpTableActions(newTableClassName);
-insertDataIntoTable(newTableClassName, users);
+// const newTableClassName = "users-table-1";
+// setUpTableActions(newTableClassName);
+// insertDataIntoTable(newTableClassName, users);
 
 const formClassName = "edit-form";
 setUpFormActions(formClassName);
-
-console.log(getUsersByInterval(0, 10));
