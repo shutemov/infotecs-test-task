76c97e8e03dc0b4606de5267b6d6bb3c5e08b4ad
Shutemov <shutemov1997@mail.ru>
2021-08-15 14:00:13 +0300

    Обновляет инициализацию в index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { insertDataIntoTable } from "./table.js";
-import { setUpTableActions } from "./setUpTable.js";
+import { setUpTable } from "./setUpTable.js";
 import { setUpFormActions } from "./setUpForm.js";
 import { setUpPagination } from "./setUpTablePagination.js";
 import { getUsersByCurrentPage } from "./tablePagination.js";
 
 //test
-setUpPagination();
+// setUpPagination();
 
 const users = getUsersByCurrentPage();
 console.log("Users from index", users);
 
 const tableClassName = "users-table";
-setUpTableActions(tableClassName);
-insertDataIntoTable(tableClassName, users);
+const numOfUsersOnPage = 10;
+setUpTable(tableClassName, numOfUsersOnPage);
 
 // const newTableClassName = "users-table-1";
 // setUpTableActions(newTableClassName);
