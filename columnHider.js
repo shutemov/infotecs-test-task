31a43bc2660ee7f627403e9aa5c570c94f95bc4f
Shutemov <shutemov1997@mail.ru>
2021-08-18 19:49:00 +0300

    Добавляет переключатель видимости столбцов в columnHider.js

--- columnHider.js
+++ columnHider.js
@@ -1,1 +1,17 @@
 
+const switchHide = (tableClassName, targetColumn, isHide) => {
+  const candidateToHideTDs = document.querySelectorAll(
+    `.${tableClassName}__${targetColumn}-body`
+  );
+
+  const candidateToHideTH = document.querySelector(
+    `.${tableClassName}__${targetColumn}-head`
+  );
+
+  const tableTDFromNodes = Array.from(candidateToHideTDs);
+  tableTDFromNodes.push(candidateToHideTH);
+
+  tableTDFromNodes.forEach((td) => {
+    td.style.display = isHide ? "none" : "";
+  });
+};
