1b96099876d95098003551557ee379692a242659
Shutemov <shutemov1997@mail.ru>
2021-08-18 19:49:49 +0300

    Добавляет логику для события change чекбоксов видимости

--- columnHider.js
+++ columnHider.js
@@ -1,3 +1,25 @@
+export const hideColumn = (tableClassName) => {
+  const hiderSection = document.querySelector(
+    `.${tableClassName}__column-hider`
+  );
+
+  hiderSection.addEventListener("change", (event) => {
+    const targetColumn = event.target.value;
+    const isHide = event.target.checked;
+
+    if (targetColumn === "first-column")
+      switchHide(tableClassName, targetColumn, isHide);
+
+    if (targetColumn === "second-column")
+      switchHide(tableClassName, targetColumn, isHide);
+
+    if (targetColumn === "third-column")
+      switchHide(tableClassName, targetColumn, isHide);
+
+    if (targetColumn === "fourth-column")
+      switchHide(tableClassName, targetColumn, isHide);
+  });
+};
 
 const switchHide = (tableClassName, targetColumn, isHide) => {
   const candidateToHideTDs = document.querySelectorAll(
