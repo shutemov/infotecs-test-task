278f79d9c3677b31c6ca767a7782a46b4e95efbb
Shutemov <shutemov1997@mail.ru>
2021-08-19 20:01:23 +0300

    Удаляет прежнюю стратегию формирования таблицы
    Переход от innerHtml текста на работу с dom через document интерфейс

--- table.js
+++ table.js
@@ -1,33 +1,8 @@
 import { colourNameToHex } from "./color.js";
 
-export const insertDataIntoTable = (targetClassName, data) => {
-  const tbody = document.querySelector(`.${targetClassName}__tbody`);
-  let content = buildContent(targetClassName, data);
-  tbody.innerHTML = content;
 };
 
-const buildContent = (targetClassName, data) => {
-  let content = "";
-  const firstColumnStyle = `${targetClassName}__first-column-body`;
-  const secondColumnStyle = `${targetClassName}__second-column-body`;
-  const thirdColumnStyle = `${targetClassName}__third-column-body`;
-  const fourthColumnStyle = `${targetClassName}__fourth-column-body`;
 
-  data.forEach((element) => {
-    const { id, name, about, eyeColor } = element;
-    const { firstName, lastName } = name;
-    const hexCodeForEyeColor = colourNameToHex(eyeColor);
-
-    content += `<tr data-id=${id}>`;
-
-    content += `<td class=${firstColumnStyle}>${firstName}</td>`;
-    content += `<td class=${secondColumnStyle}>${lastName}</td>`;
-    content += `<td class=${thirdColumnStyle}> ${about} </td>`;
-    content += `<td class=${fourthColumnStyle}>`;
-    content += `<input type="color" value=${hexCodeForEyeColor} disabled/>`;
-    content += `</td>`;
-    content += `</tr>`;
   });
 
-  return content;
 };
