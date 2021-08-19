63866c282a330579b32d87560e1bb2d8476b120d
Shutemov <shutemov1997@mail.ru>
2021-08-19 12:49:31 +0300

    Добавляет функцию создания пагинации

--- pagination.js
+++ pagination.js
@@ -13,21 +13,46 @@ import { getNumberOfUser } from "./data.js";
 export const setPage = (_page) => {
   page = _page;
 const paginations = [];
+
+export const createPagination = (tableClassName) => {
+  const isExistPagination = getPagination(tableClassName)
+  if(isExistPagination) return
+
+  const pagination = {
+    table: tableClassName,
+    state: {
+      page: 0,
+      numOfEntries: 1,
+    },
+  };
+
+  paginations.push(pagination);
+};
+  const pagination = getPagination(tableClassName);
+  pagination.state.page = page;
 };
 
 export const setNumOfEntries = (num) => {
   numOfEntries = num;
+  const pagination = getPagination(tableClassName);
+  pagination.state.numOfEntries = num;
 };
 
 export const getCurrentPage = () => {
   return page;
+  const pagination = getPagination(tableClassName);
+  return pagination.state.page;
 };
 
 export const getNumOfEntries = () => {
   return numOfEntries;
+  const pagination = getPagination(tableClassName);
+  return pagination.state.numOfEntries;
 };
 
 export const getTotalNumOfPages = () => {
+  const pagination = getPagination(tableClassName);
+  const numOfEntries = pagination.state.numOfEntries;
   const numOfUsers = getNumberOfUser();
   const totalNumOfPages = numOfUsers / numOfEntries;
   return totalNumOfPages;
@@ -36,9 +61,12 @@ export const getTotalNumOfPages = () => {
 export const increasePage = (num) => {
   page += num ? num : 1;
   return page;
+  pagination.state.page += num ? num : 1;
+  return pagination.state.page;
 };
 
-export const decreasePage = (num) => {
-  page -= num ? num : 1;
-  return page;
+export const decreasePage = (tableClassName, num) => {
+  const pagination = getPagination(tableClassName);
+  pagination.state.page -= num ? num : 1;
+  return pagination.state.page;
 };
