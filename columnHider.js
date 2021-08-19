3be8f60ec4c7446b06c3835459939740c0e6fc73
Shutemov <shutemov1997@mail.ru>
2021-08-19 19:56:36 +0300

    Удаляет switchColumnHiding из columnHider.js

--- columnHider.js
+++ columnHider.js
@@ -12,17 +12,8 @@ const columnHiders = [];
 
 };
 
-const switchColumnHiding = (tableClassName, targetColumn, isHide) => {
-  const candidateToHideTDs = document.querySelectorAll(
-    `.${tableClassName}__${targetColumn}-body`
-  );
 
-  const candidateToHideTH = document.querySelector(
-    `.${tableClassName}__${targetColumn}-head`
-  );
 
-  const tableTDFromNodes = Array.from(candidateToHideTDs);
-  tableTDFromNodes.push(candidateToHideTH);
 
 const getColumnHider = (tableClassName) => {
   return columnHiders.find((columnHider) => {
