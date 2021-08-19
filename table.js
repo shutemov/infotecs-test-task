765ff7827bda8bfba08ae58080803273b6446038
Shutemov <shutemov1997@mail.ru>
2021-08-19 20:02:25 +0300

    Обновляет insertDataIntoTable в table.js

--- table.js
+++ table.js
@@ -1,6 +1,8 @@
 import { colourNameToHex } from "./color.js";
 import { getHiddenColumns } from "./columnHider.js";
 
+export const insertDataIntoTable = (tableClassName, data) => {
+  buildContent(tableClassName, data);
 };
 
 
