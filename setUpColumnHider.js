1685d77c9e8297802173d36e8b400ec6c8d96b40
Shutemov <shutemov1997@mail.ru>
2021-08-19 20:26:39 +0300

    Создает setUpColumnHider.js
    Переносит ответственности за работу с евентами на из модуля columnHider.js в setUpColumnHider.js

---
+++ setUpColumnHider.js
@@ -0,0 +1,36 @@
+import { addHiddenColumn, removeHiddenColumn } from "./columnHider.js";
+import { getUsersByCurrentPage } from "./data.js";
+import { insertDataIntoTable } from "./table.js";
+
+export const hideColumn = (tableClassName) => {
+  const hiderSection = document.querySelector(
+    `.${tableClassName}__column-hider`
+  );
+
+  hiderSection.addEventListener("change", (event) => {
+    const column = event.target.value;
+    const isChecked = event.target.checked;
+
+    if (!isChecked) {
+      removeHiddenColumn(tableClassName, column);
+      updateTableAndHeader(tableClassName, column, isChecked);
+      return;
+    }
+    addHiddenColumn(tableClassName, column);
+    updateTableAndHeader(tableClassName, column, isChecked);
+  });
+};
+
+const updateTableAndHeader = (tableClassName, column, isChecked) => {
+  const users = getUsersByCurrentPage(tableClassName);
+  insertDataIntoTable(tableClassName, users);
+  switchColumnHeaderVisible(tableClassName, column, isChecked);
+};
+
+const switchColumnHeaderVisible = (tableClassName, selectedColumn, isHide) => {
+  const candidateToHideTH = document.querySelector(
+    `.${tableClassName}_${selectedColumn}-column-head`
+  );
+  const visible = isHide ? "none" : "";
+  candidateToHideTH.style.display = visible;
+};
