ae7f9b99a5ada92a402323c8473cb94ce9b39349
Shutemov <shutemov1997@mail.ru>
2021-08-19 20:22:03 +0300

    Комментирует прежнюю стратегию формирования таблицы

--- table.js
+++ table.js
@@ -75,3 +75,35 @@ export const insertDataIntoTable = (tableClassName, data) => {
     tbody.appendChild(tr);
   });
 };
+
+// export const insertDataIntoTable = (tableClassName, data) => {
+//   const tbody = document.querySelector(`.${tableClassName}__tbody`);
+//   let content = buildContent(tableClassName, data);
+//   tbody.innerHTML = content;
+// };
+
+// const buildContent = (tableClassName, data) => {
+//   let content = "";
+//   const firstColumnStyle = `${tableClassName}__a-column-body`;
+//   const secondColumnStyle = `${tableClassName}__b-column-body`;
+//   const thirdColumnStyle = `${tableClassName}__c-column-body`;
+//   const fourthColumnStyle = `${tableClassName}__d-column-body`;
+
+//   data.forEach((element) => {
+//     const { id, name, about, eyeColor } = element;
+//     const { firstName, lastName } = name;
+//     const hexCodeForEyeColor = colourNameToHex(eyeColor);
+
+//     content += `<tr data-id=${id}>`;
+
+//     content += `<td class=${firstColumnStyle}>${firstName}</td>`;
+//     content += `<td class=${secondColumnStyle}>${lastName}</td>`;
+//     content += `<td class=${thirdColumnStyle}> ${about} </td>`;
+//     content += `<td class=${fourthColumnStyle}>`;
+//     content += `<input type="color" value=${hexCodeForEyeColor} disabled/>`;
+//     content += `</td>`;
+//     content += `</tr>`;
+//   });
+
+//   return content;
+// };
