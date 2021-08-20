03df29bb55987ab40cf7af3f7072ca64f8c63538
Shutemov <shutemov1997@mail.ru>
2021-08-20 07:48:10 +0300

    Обновляет setUpForm.js
    Вынесены функции работающие с представлением формы

--- setUpForm.js
+++ setUpForm.js
@@ -1,34 +1,36 @@
-import { getDataFromForm, clearForm } from "./form.js";
-import {
-  getAllUsers,
-  getUserById,
-  getUsersByCurrentPage,
-  updateUser,
-} from "./data.js";
+import { getDataFromForm, clearFormData } from "./form.js";
+import { getUserById, getUsersByCurrentPage, updateUser } from "./data.js";
 import { insertDataIntoTable } from "./table.js";
+import { getColorsNames } from "./color.js";
 
 export const setUpFormActions = (formClassName, tableClassName) => {
+
+const setUpFormEvents = (tableClassName, formClassName) => {
   if (!formClassName)
     throw new Error(`Form class name: ${formClassName} is not defined`);
 
-  setUpFormSaveButtonAction(`.edit-form__save-button`, tableClassName);
+  const saveButtonClassName = `edit-form__save-button`;
+  setUpSaveButtonEvent(tableClassName, saveButtonClassName);
 };
 
-const setUpFormSaveButtonAction = (buttonClassName, tableClassName) => {
-  const saveButton = document.querySelector(buttonClassName);
+const setUpSaveButtonEvent = (tableClassName, buttonClassName) => {
+  const saveButton = document.querySelector(
+    `.${tableClassName}__edit-form > .edit-form__item > .${buttonClassName}`
+  );
 
   saveButton.addEventListener("click", () => {
     const storage = window.localStorage;
     const id = storage.getItem("userId");
 
     if (!id) {
-      alert("user not selected");
+      alert("User not selected");
       return;
     }
 
     const user = getUserById(id);
 
-    const { firstName, lastName, about, eyeColor } = getDataFromForm();
+    const { firstName, lastName, about, eyeColor } =
+      getDataFromFormFields(tableClassName);
 
     user.name.firstName = firstName;
     user.name.lastName = lastName;
@@ -36,9 +38,86 @@ const setUpFormSaveButtonAction = (buttonClassName, tableClassName) => {
     user.eyeColor = eyeColor;
 
     updateUser(user);
+
     const users = getUsersByCurrentPage(tableClassName);
-    insertDataIntoTable("users-table", users);
-    clearForm();
+    insertDataIntoTable(tableClassName, users);
+
+    //clear form after using
+    clearFormData(tableClassName);
+    clearFormFields(tableClassName);
     storage.clear();
   });
+};
+
+export const getDataFromFormFields = (tableClassName) => {
+  const [firstNameInput, lastNameInput, aboutTextarea, eyeColorSelect] =
+    getInputFields(tableClassName);
+
+  const data = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    about: aboutTextarea.value,
+    eyeColor: eyeColorSelect.value,
+  };
+
+  return data;
+};
+
+export const clearFormFields = (tableClassName) => {
+  const [firstNameInput, lastNameInput, aboutTextarea, eyeColorSelect] =
+    getInputFields(tableClassName);
+
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  aboutTextarea.value = "";
+  eyeColorSelect.value = "";
+};
+
+export const setDataIntoFormFields = (tableClassName) => {
+  const { firstName, lastName, about, eyeColor } =
+    getDataFromForm(tableClassName);
+
+  const [firstNameInput, lastNameInput, aboutTextarea, eyeColorInput] =
+    getInputFields(tableClassName);
+
+  firstNameInput.value = firstName;
+  lastNameInput.value = lastName;
+  aboutTextarea.value = about;
+  eyeColorInput.value = eyeColor;
+};
+
+/*
+  Каждый 3 элемент item'a содержит значение
+*/
+const getInputFields = (tableClassName) => {
+  const nodeListEditFormItems = document.querySelectorAll(
+    `.${tableClassName}__edit-form > .edit-form__item`
+  );
+
+  const formInputFields = Array.from(nodeListEditFormItems).map((item) => {
+    return item.childNodes[3];
+  });
+
+  return formInputFields;
+};
+
+export const initEyeColorSelect = (tableClassName, formClassName) => {
+  const select = document.querySelector(
+    `.${tableClassName}__edit-form > .edit-form__item > .${formClassName}__eye-color-select`
+  );
+  const allColorNames = getColorsNames();
+
+  allColorNames.forEach((color) => {
+    insertOption(select, color);
+  });
+};
+
+const insertOption = (select, value) => {
+  //create option
+  const option = document.createElement("option");
+  option.value = value;
+  option.innerHTML = value;
+
+  //insert
+  select.appendChild(option);
 };
