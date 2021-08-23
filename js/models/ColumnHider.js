4ca143deffc8e820cd24c1f7bcc8ef1d083540cb
Shutemov <shutemov1997@mail.ru>
2021-08-23 09:31:55 +0300

    Создает класс ColumnHider

---
+++ js/models/ColumnHider.js
@@ -0,0 +1,29 @@
+export class ColumnHider {
+  constructor(tableClassName) {
+    this.table = tableClassName;
+    this.hiddenColumns = [];
+  }
+
+  addHiddenColumn(column) {
+    const hasColumn = this.hiddenColumns.includes(column);
+    if (hasColumn) return;
+
+    this.hiddenColumns.push(column);
+  }
+
+  removeHiddenColumn(column) {
+    const hiddenColumns = this.hiddenColumns.filter((hiddenColumn) => {
+      return hiddenColumn !== column;
+    });
+
+    this.hiddenColumns = hiddenColumns;
+  }
+
+  getTable() {
+    return this.table;
+  }
+
+  getHiddenColumns() {
+    return this.hiddenColumns;
+  }
+}
