7e8931a42b0f1aceed902fb0e0fdba12452a2f87
Shutemov <shutemov1997@mail.ru>
2021-08-23 09:32:50 +0300

    Переводит columnHider.js на работу с классом ColumnHider

--- js/columnHider.js
+++ js/columnHider.js
@@ -5,50 +5,44 @@
   }
 */
 
+import { ColumnHider } from "./models/ColumnHider.js";
+
 const columnHiders = [];
 
 export const createColumnHiderModel = (tableClassName) => {
   const isExistColumnHider = getColumnHider(tableClassName);
   if (isExistColumnHider) return;
-
-  const columnHider = {
-    table: tableClassName,
-    hiddenColumns: [],
-  };
+  
+  const columnHider = new ColumnHider(tableClassName);
 
   columnHiders.push(columnHider);
 };
 
 export const addHiddenColumn = (tableClassName, column) => {
   const columnHider = getColumnHider(tableClassName);
-  const hasColumn = columnHider.hiddenColumns.includes(column);
-
-  if (hasColumn) return;
-
-  columnHider.hiddenColumns.push(column);
+  columnHider.addHiddenColumn(column);
 };
 
 export const removeHiddenColumn = (tableClassName, column) => {
   const columnHider = getColumnHider(tableClassName);
-  
-  const hiddenColumns = columnHider.hiddenColumns.filter((hiddenColumn) => {
-    return hiddenColumn !== column;
-  });
-  columnHider.hiddenColumns = hiddenColumns;
+  columnHider.removeHiddenColumn(column);
 };
 
 export const getHiddenColumns = (tableClassName) => {
-  const { hiddenColumns } = getColumnHider(tableClassName);
+  const columnHider = getColumnHider(tableClassName);
+  const hiddenColumns = columnHider.getHiddenColumns();
   return hiddenColumns;
 };
 
 export const isColumnHidden = (tableClassName, column) => {
-  const { hiddenColumns } = getColumnHider(tableClassName);
+  const columnHider = getColumnHider(tableClassName);
+  hiddenColumns = columnHider.getHiddenColumns();
   return hiddenColumns.includes(column);
 };
 
 const getColumnHider = (tableClassName) => {
   return columnHiders.find((columnHider) => {
-    return columnHider.table === tableClassName;
+    const tartgetTableClassName = columnHider.getTable();
+    return tartgetTableClassName === tableClassName;
   });
 };
