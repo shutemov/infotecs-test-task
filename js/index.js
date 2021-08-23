41760de3295e51f5c804d220fa78e6348aca56c5
Shutemov <shutemov1997@mail.ru>
2021-08-23 08:14:15 +0300

    Обновляет инициализацию в index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ import { createPaginationModel } from "./pagination.js";
 import { initColumnHider } from "./setUpColumnHider.js";
 import { createColumnHiderModel } from "./columnHider.js";
 
+/*
+    1st table
+*/
+
 const tableClassName = "users-table";
 
 //create models
@@ -20,4 +24,21 @@ initColumnHider(tableClassName);
 const formClassName = "edit-form";
 initForm(tableClassName, formClassName);
 
+/*
+    2nd table
+*/
 
+// const secondTableClassName = "users-table-1";
+
+// //create models
+// createPaginationModel(secondTableClassName);
+// createColumnHiderModel(secondTableClassName);
+// createFormModel(secondTableClassName);
+
+// //init's
+// const numOfUsersOnPageForSecondTable = 5;
+// initTable(secondTableClassName, numOfUsersOnPageForSecondTable);
+
+// initColumnHider(secondTableClassName);
+// const formClassNameForSecondTable = "edit-form";
+// initForm(secondTableClassName, formClassNameForSecondTable);
